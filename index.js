fffbc19e2a7be1e758ede9a0d7f950af36dca59b
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-05-07 15:45:18 +0600

    chore: add package.json with initial dependencies for simple CRUD server

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://simpleCrudUser:xsSZwTOLtg9EsOYc@cluster0.cio91mj.mongodb.net/?appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const db = client.db('simpleCrud');
+        const userCollection = db.collection('users')
+
+        app.get('/users',async(req,res)=>{
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        await client.db('admin').command({ ping: 1 })
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        
+    } finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Simple CRUD Server is serving')
+})
+
+
+app.listen(port, () => {
+    console.log(`Simple CRUD server is running on port ${port}`);
+
+})
