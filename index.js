ddde038337e572ee7354e763b4a709e11b71e63b
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-05-08 00:42:28 +0600

    feat: implement post,delete,read,patch operations for users in the simple CRUD server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors());
@@ -26,16 +26,62 @@ const run = async () => {
         const db = client.db('simpleCrud');
         const userCollection = db.collection('users')
 
-        app.get('/users',async(req,res)=>{
+        app.get('/users', async (req, res) => {
             const cursor = userCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const user = await userCollection.findOne(query);
+
+            console.log('User Id: ', id);
+
+            res.send(user)
+        })
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            // console.log('user to be inserted: ', newUser);
+            
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        app.patch('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const modifiedUser = req.body;
+            const updatedDocument = {
+                $set: {
+                    name: modifiedUser.name,
+                    email: modifiedUser.email,
+                    role: modifiedUser.email,
+                }
+            }
+            const result = await userCollection.updateOne(filter,updatedDocument);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
         await client.db('admin').command({ ping: 1 })
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        
+
     } finally {
         // await client.close();
     }
